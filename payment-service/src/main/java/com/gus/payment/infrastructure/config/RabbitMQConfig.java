32de07b897b568faa6b7f289929a26c9ad4a19d9
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2026-01-14 17:24:43 -0300

    build(infra): add RabbitMQ support and docker configuration

---
+++ payment-service/src/main/java/com/gus/payment/infrastructure/config/RabbitMQConfig.java
@@ -0,0 +1,39 @@
+package com.gus.payment.infrastructure.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String PAYMENT_EXCHANGE = "payment.events";
+    public static final String PAYMENT_CREATED_QUEUE = "payment.created.queue";
+    public static final String ROUTING_KEY = "payment.created";
+
+    @Bean
+    public Queue paymentCreatedQueue() {
+        // durable = true (se o RabbitMQ reiniciar, a fila não some)
+        return new Queue(PAYMENT_CREATED_QUEUE, true);
+    }
+
+    @Bean
+    public TopicExchange paymentExchange() {
+        return new TopicExchange(PAYMENT_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
